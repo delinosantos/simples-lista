a4d8be35f8d1d110683603285deebe4e49cc7d5e
delinosantos <delino_10@hotmail.com>
2022-07-26 12:22:29 -0300

    commit list

---
+++ script.js
@@ -0,0 +1,14 @@
+function adicionar(){
+
+    let text = document.getElementById("texto").value;
+    let list = document.getElementById("lista").innerHTML;
+
+
+    list += "<li>" +text+ "</li>"
+
+    document.getElementById("lista").innerHTML = list;
+
+    document.getElementById("texto").value = null;
+
+
+}
